1c930e1ae1a353cd3f363a0c8acb2f513904e013
Nicolas Biri <nicolas.biri@list.lu>
2016-03-03 09:31:51 +0100

    modify the way to import libs to satisfy browserify

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 
 /* jsmf.core reexports */
 
+var libs =
+  [ require('jsmf-core')
+  , require('jsmf-jstl')
+  , require('jsmf-magellan')
+  , require('jsmf-neo4j')
+  , require('jsmf-json')
+  , require('jsmf-check')
+  , require('jsmf-util')
+  ];
+
 (function() {
-    var libnames = ['jsmf-core', 'jsmf-jstl', 'jsmf-magellan', 'jsmf-neo4j', 'jsmf-json', 'jsmf-check', 'jsmf-util'];
-    for (var i in libnames) {
-        var lib = require(libnames[i]);
+    for (var i in libs) {
+        var lib = libs[i];
         for (var k in lib) {
           module.exports[k] = lib[k];
         }
